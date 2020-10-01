ce35347e532493d050e04e6873b71964a4b5c80a
Favour Okonta <HP@DESKTOP-4AVRTVM.ligmus360.com>
2020-10-01 20:04:08 +0100

    updated code

--- security_grp.tf
+++ security_grp.tf
@@ -1,12 +1,18 @@
 resource "aws_security_group" "demo_sg" {
-  name = "terraform_created_sg"
+  name = "toolbox_created_sg"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
@@ -14,5 +20,4 @@ resource "aws_security_group" "demo_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
